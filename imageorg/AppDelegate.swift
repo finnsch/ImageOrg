9b19d3d425624f5f9c655370c3b0828dd4812893
Finn Schlenk <finn@schlenk.biz>
2018-10-21 16:30:47 +0200

    Initial Commit

---
+++ imageorg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  imageorg
+//
+//  Created by Finn Schlenk on 21.10.18.
+//  Copyright © 2018 Finn Schlenk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ imageorg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  imageorg
+//
+//  Created by Finn Schlenk on 21.10.18.
+//  Copyright © 2018 Finn Schlenk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
